491781186a5269f113cf16fb8b90d5d13f2177ef
Misha Zinchenko <m.zinchenko@rubarbs.com>
2019-11-11 17:30:08 +0200

    base validate

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -6,14 +6,23 @@ use App\Entity\Student;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class StudentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', null, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 2, 'max' => 20])],
+                'help' => 'First name is required'
+            ])
+            ->add('lastName', null, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 2, 'max' => 20])],
+                'help' => 'Last name is required'
+            ])
             ->add('course')
         ;
     }
