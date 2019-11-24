22d6e8e715c07c4f8273059d161c5fd1d8729b8b
zinchenko1 <aswad1909@gmail.com>
2019-11-24 23:57:43 +0200

    add user

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+use App\Repository\StudentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class SiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="site_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+}
